837841afe423ba6f001afaef992bcb841cb254f7
Felix Ravella <felix.ravella@gmail.com>
2024-06-18 16:27:29 +0200

    test lb 3

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,9 +2,9 @@ const request = require("supertest");
 const app = require("../index"); // Adjust the path as needed
 
 describe("GET /", () => {
-  it("should return Hello World! tentative 2", async () => {
+  it("should return Hello World! tentative 3", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe("Hello World! tentative 2");
+    expect(res.text).toBe("Hello World! tentative 3");
   });
 });
