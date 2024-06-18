2b0180997f8fb17c4d6def0de48e3f1817f16095
Felix Ravella <felix.ravella@gmail.com>
2024-06-18 16:41:11 +0200

    test lb 5

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! tentative 4");
+  res.send("Hello World! tentative 5");
 });
 
 if (process.env.NODE_ENV !== "test") {
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index"); // Adjust the path as needed
 
 describe("GET /", () => {
-  it("should return Hello World! tentative 4", async () => {
+  it("should return Hello World! tentative 5", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
     expect(res.text).toBe("Hello World! tentative 4");
